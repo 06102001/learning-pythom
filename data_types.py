26690ed0d0437f6541d7898ba3100306ebc71f7f
David C <moskeimar@gmail.com>
2024-01-20 12:06:51 -0500

    First Commit data_types

---
+++ data_types.py
@@ -0,0 +1,36 @@
+#Tipos de datos en Python
+#Python es un lenguaje dinanmicamente tipado
+
+#1. Numericos
+## Enteros Z =>int
+num1 = 42
+print("Num1:", type(num1))
+## Flotantes o decimales (reales) => float
+num2 = 50.5
+print("Num2:", type(num2))
+## Complejos => complex
+num3 = 2j
+print("Num3:", type(num3))
+
+#2. Cadena => str
+my_name = "David"
+print("my_name", type(my_name))
+my_lastname = 'Mosquera'
+print("my_lastname", type(my_lastname))
+profile = '''David es una persona enfocada en
+el trabajo en equipo'''
+print("profile", type(profile))
+adress = 'Casa 21 A'
+print("adress", type(adress))
+
+#3. Logicos (valores o expresiones booleanas) => bool
+usuario_activo = True
+print("usuario_activo", type(usuario_activo))
+
+#4. Listas
+frutas = ['Apple', 'Banana']
+print(frutas)
+print("Frutas", type(frutas))
+#5. Tuplas
+#6. Diccionarios
+#7. Conjuntos
