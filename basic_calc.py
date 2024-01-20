2be92fa4c92cda681feda813a19f599ca31fcf9b
David C <moskeimar@gmail.com>
2024-01-20 09:21:09 -0500

    First commit basic_calc script

---
+++ basic_calc.py
@@ -0,0 +1,35 @@
+#Script title: Basic Calculator V1
+'''Developer  name: David Camilo Mosquera
+Date: 18-01-2024'''
+
+''''DESCRIPTIONS:
+This calculator let you get the result of 4 basics
+math operations from two numbers (Variables).
+'''
+
+#INPUTS (Two variables)
+num1 = 20
+num2 = 5
+
+#PROCESS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#OUTPUTS
+print("::::BASIC CALC V1.0:::")
+print("Number 1:", num1, type(num1))
+print("number 2:", num2, type(num2))
+print("The addition is:", add)
+print("The subtraction is:", sub)
+print("The multiplication is:", mul)
+print("The division is:", div)
+print("\n")
+print("::::BASIC CALC V1.2:::")
+print("Number 1:", num1, type(num1))
+print("number 2:", num2, type(num2))
+print("The addition is:", num1 + num2)
+print("The subtraction is:", num1 - num2)
+print("The multiplication is:", num1 * num2)
+print("The division is:", num1 / num2)
